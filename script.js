ab242922d63eac4f477771d091057c7ee190b730
eduardobandeira57 <eduapp196@gmail.com>
2024-12-11 19:11:05 -0300

    Add files via upload
    subi  o html e o css e o js

---
+++ script.js
@@ -0,0 +1,112 @@
+function pra1(){
+    document.getElementById("apresenta").style.display="block"
+    document.getElementById("respost").textContent="E afastar-se-á a inveja de Efraim, e os adversários de Judá serão desarraigados; Efraim não invejará a Judá, e Judá não oprimirá a Efraim.";
+    document.getElementById("voltar").textContent="voltar"
+}
+function pra2(){
+    document.getElementById("apresenta").style.display="block"
+    document.getElementById("respost").textContent="Com alegria vocês tirarão águas das fontes da salvação.";
+    document.getElementById("voltar").textContent="voltar"
+}
+function pra3(){
+    document.getElementById("apresenta").style.display="block"
+    document.getElementById("respost").textContent=" Ó Senhor, esperança de Israel, todos aqueles que te deixam serão envergonhados; os que se apartam de mim serão escritos sobre a terra; porque abandonam o Senhor, a fonte das águas vivas.";
+    document.getElementById("voltar").textContent="voltar"
+}
+function pra4(){
+    document.getElementById("apresenta").style.marginTop = "150px";
+    document.getElementById("apresenta").style.display="block"
+    document.getElementById("respost").textContent="29 Porém cada nação fez os seus deuses, e os puseram nas casas dos altos que os samaritanos fizeram, cada nação nas cidades, em que habitava. 30 E os de babilônia fizeram Sucote-Benote; e os de Cuta fizeram Nergal; e os de Hamate fizeram Asima. 31 E os aveus fizeram Nibaz e Tartaque; e os sefarvitas queimavam seus filhos no fogo a Adrameleque, e a Anameleque, deuses de Sefarvaim.";
+    document.getElementById("voltar").textContent="voltar"
+    document.getElementById("apresenta").style.marginTop = "200px";
+
+}
+function pra5(){
+    document.getElementById("apresenta").style.display="block"
+    document.getElementById("respost").textContent=" 1– cultuavam uma deusa chamada Sucote-Benote.Cuta. 2– cultuavam o deus Nergal.Ava.  3– cultuavam deuses Nibaz eTartaqueHamate.   4– cultuavam a deusa Asima.Sefarvaim.  s5– cultuavam os deuses Adrameleque e Anameleque";
+    document.getElementById("voltar").textContent="voltar"
+    document.getElementById("apresenta").style.marginTop = "200px";
+
+}
+function pra6(){
+    document.getElementById("apresenta").style.marginTop = "310px";
+    document.getElementById("apresenta").style.display="block"
+    document.getElementById("respost").textContent=" Fui buscado dos que não perguntavam por mim, fui achado daqueles que não me buscavam; a uma nação que não se chamava do meu nome eu disse: Eis-me aqui. Eis-me aqui.";
+    document.getElementById("voltar").textContent="voltar"
+}
+function pra7(){
+    document.getElementById("apresenta").style.marginTop = "470px";
+    document.getElementById("apresenta").style.display="block"
+    document.getElementById("respost").textContent="29 Sucedeu, pois, naquele tempo que, saindo Jeroboão de Jerusalém, o profeta Aías, o silonita, o encontrou no caminho, e ele estava vestido com uma roupa nova, e os dois estavam sós no campo.  30 E Aías pegou na roupa nova que tinha sobre si, e a rasgou em doze pedaços.  31 E disse a Jeroboão: Toma para ti os dez pedaços, porque assim diz o Senhor Deus de Israel: Eis que rasgarei o reino da mão de Salomão, e a ti darei as dez tribos.";
+    document.getElementById("voltar").textContent="voltar"
+}
+function pra8(){
+    document.getElementById("apresenta").style.marginTop = "600px";
+    document.getElementById("apresenta").style.display="block"
+    document.getElementById("respost").textContent="1 Quanto aos filhos de Rúben, o primogênito de Israel (pois ele era o primogênito; mas porque profanara a cama de seu pai, deu-se a sua primogenitura aos filhos de José, filho de Israel; de modo que não foi contado, na genealogia da primogenitura,  2 Porque Judá foi poderoso entre seus irmãos, e dele veio o soberano; porém a primogenitura foi de José).";
+    document.getElementById("voltar").textContent="voltar"
+}
+function pra9(){
+    document.getElementById("apresenta").style.marginTop = "700px";
+    document.getElementById("apresenta").style.display="block"
+    document.getElementById("respost").textContent="Mas Israel estendeu a sua mão direita e a pôs sobre a cabeça de Efraim, que era o menor, e a sua esquerda sobre a cabeça de Manassés, dirigindo as suas mãos propositadamente, não obstante Manassés ser o primogênito.";
+    document.getElementById("voltar").textContent="voltar"
+}
+function pra10(){
+    document.getElementById("apresenta").style.marginTop = "800px";
+    document.getElementById("apresenta").style.display="block"
+    document.getElementById("respost").textContent="17 Vendo, pois, José que seu pai punha a sua mão direita sobre a cabeça de Efraim, foi mau aos seus olhos; e tomou a mão de seu pai, para a transpor de sobre a cabeça de Efraim à cabeça de Manassés.  18 E José disse a seu pai: Não assim, meu pai, porque este é o primogênito; põe a tua mão direita sobre a sua cabeça.  19 Mas seu pai recusou, e disse: Eu o sei, meu filho, eu o sei; também ele será um povo, e também ele será grande; contudo o seu irmão menor será maior que ele, e a sua descendência será uma multidão de nações.";
+    document.getElementById("voltar").textContent="voltar"
+}
+
+function pra11(){
+    document.getElementById("apresenta").style.marginTop = "900px";
+    document.getElementById("apresenta").style.display="block"
+    document.getElementById("respost").textContent="8 Porém a cabeça da Síria será Damasco, e a cabeça de Damasco Rezim; e dentro de sessenta e cinco anos Efraim será destruído, e deixará de ser povo. ";
+    document.getElementById("voltar").textContent="voltar"
+}
+function pra12(){
+    document.getElementById("apresenta").style.marginTop = "1200px";
+    document.getElementById("apresenta").style.display="block"
+    document.getElementById("respost").textContent="   9 Entretanto a cabeça de Efraim será Samaria, e a cabeça de Samaria o filho de Remalias; se não o crerdes, certamente não haveis de permanecer.";
+    document.getElementById("voltar").textContent="voltar"
+}
+function pra13(){
+    document.getElementById("apresenta").style.marginTop = "1200px";
+    document.getElementById("apresenta").style.display="block"
+    document.getElementById("respost").textContent="19 Mas seu pai recusou, e disse: Eu o sei, meu filho, eu o sei; também ele será um povo, e também ele será grande; contudo o seu irmão menor será maior que ele, e a sua descendência será uma multidão de nações.";
+    document.getElementById("voltar").textContent="voltar"
+}
+function pra14(){
+    document.getElementById("apresenta").style.marginTop = "1340px";
+    document.getElementById("apresenta").style.display="block"
+    document.getElementById("respost").textContent="11 E os filhos de Judá e os filhos de Israel juntos se congregarão, e constituirão sobre si uma só cabeça, e subirão da terra; porque grande será o dia de Jizreel. (Jizreel traduzindo o dia que Deus semeia)";
+    document.getElementById("voltar").textContent="voltar"
+}
+function pra15(){
+    document.getElementById("apresenta").style.marginTop = "1360px";
+    document.getElementById("apresenta").style.display="block"
+    document.getElementById("respost").textContent="19 Disse-lhe a mulher: Senhor, vejo que és profeta.  20 Nossos pais adoraram neste monte, e vós dizeis que é em Jerusalém o lugar onde se deve adorar.  21 Disse-lhe Jesus: Mulher, crê-me que a hora vem, em que nem neste monte nem em Jerusalém adorareis o Pai.  22 Vós adorais o que não sabeis; nós adoramos o que sabemos porque a salvação vem dos judeus.  23 Mas a hora vem, e agora é, em que os verdadeiros adoradores adorarão o Pai em espírito e em verdade; porque o Pai procura a tais que assim o adorem.  24 Deus é Espírito, e importa que os que o adoram o adorem em espírito e em verdade.  25 A mulher disse-lhe: Eu sei que o Messias (que se chama o Cristo) vem; quando ele vier, nos anunciará tudo.  26 Jesus disse-lhe: Eu o sou, eu que falo contigo.";
+    document.getElementById("voltar").textContent="voltar"
+}
+function pra16(){
+    document.getElementById("apresenta").style.marginTop = "240px";
+    document.getElementById("apresenta").style.display="block"
+    document.getElementById("respost").textContent="";
+    document.getElementById("voltar").textContent="voltar"
+}
+function pra17(){
+    document.getElementById("apresenta").style.marginTop = "240px";
+    document.getElementById("apresenta").style.display="block"
+    document.getElementById("respost").textContent="";
+    document.getElementById("voltar").textContent="voltar"
+}
+
+
+function voltar(){
+    document.getElementById("apresenta").style.display="none"
+    document.getElementById("respost").textContent="";
+    document.getElementById("voltar").textContent=""
+
+
+}
